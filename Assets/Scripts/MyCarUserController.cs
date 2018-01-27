db6774b1ecd3b1799bb46e7eff8a8fc9abcd303c
Sawyer Warden <sawyer.warden@gmail.com>
2018-01-27 11:10:14 -0500

    Player Manager
    Player manager for maintaining players/spawning

---
+++ Assets/Scripts/MyCarUserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Vehicles.Car;
+
+[RequireComponent(typeof (CarController))]
+
+public class MyCarUserControl : MonoBehaviour
+{
+	
+	public string id;
+	private CarController m_Car; // the car controller we want to use
+
+	public MyCarUserControl(string id_){
+		id = id_;
+	}
+
+	private void Awake()
+	{
+		// get the car controller
+		m_Car = GetComponent<CarController>();
+	}
+
+	private void FixedUpdate()
+	{
+		// pass the input to the car!
+		float h = CrossPlatformInputManager.GetAxis(id + "Horizontal");
+		float v = CrossPlatformInputManager.GetAxis(id + "Vertical");
+		#if !MOBILE_INPUT
+		float handbrake = CrossPlatformInputManager.GetAxis(id + "Jump");
+		m_Car.Move(h, v, v, handbrake);
+		#else
+		m_Car.Move(h, v, v, 0f);
+		#endif
+	}
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class PlayerManager : MonoBehaviour {
+
+	public int numPlayers = 4;
+	//List<CarUserControl> playerList = new List<CarUserControl> ();
+	public List<GameObject> playersInScene = new List<GameObject> ();
+	public List<GameObject> spawnPoints = new List<GameObject> ();
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < numPlayers; i++) {
+			GameObject o = Instantiate (Resources.Load ("truck", typeof(GameObject))) as GameObject;
+			string id = "P" + (i + 1);
+
+			o.GetComponent<MyCarUserControl> ().id = id;
+			o.name = id;
+
+			int pick = Random.Range (0, numPlayers);
+			while(!spawnPoints [pick].GetComponent<SpawnPointStats> ().open) {
+				pick = Random.Range (0, numPlayers);
+			}
+
+			o.transform.position = spawnPoints [pick].transform.position;
+			playersInScene.Add (o);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
